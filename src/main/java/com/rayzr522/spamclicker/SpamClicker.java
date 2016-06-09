6f13b22c1b94aedc414d3d9b416f69d49185461e
Peter Blood <rayzr522@users.noreply.github.com>
2016-06-09 07:54:27 -0400

    Update v1.1
    Fixed player’s inventory and position getting cleared on re-log

--- src/main/java/com/rayzr522/spamclicker/SpamClicker.java
+++ src/main/java/com/rayzr522/spamclicker/SpamClicker.java
@@ -6,7 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SpamClicker extends JavaPlugin implements Listener {
@@ -26,7 +26,7 @@ public class SpamClicker extends JavaPlugin implements Listener {
 	}
 
 	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onPlayerLogIn(PlayerLoginEvent e) {
+	public void onPlayerLogIn(PlayerJoinEvent e) {
 
 		Player p = e.getPlayer();
 		p.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(1024);
