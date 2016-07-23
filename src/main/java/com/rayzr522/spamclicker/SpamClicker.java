7fc276ed2545a0c834e05b99b73662437ca59f32
Rayzr522 <rayzr522@users.noreply.github.com>
2016-07-23 13:51:21 -0400

    Added option for re-enabling the attack cooldown

--- src/main/java/com/rayzr522/spamclicker/SpamClicker.java
+++ src/main/java/com/rayzr522/spamclicker/SpamClicker.java
@@ -1,7 +1,14 @@
 
 package com.rayzr522.spamclicker;
 
+import java.io.File;
+
+import org.bukkit.Bukkit;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -9,13 +16,40 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class SpamClicker extends JavaPlugin implements Listener {
+import com.google.common.base.Strings;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class SpamClicker extends JavaPlugin implements Listener, CommandExecutor {
+
+	private static final String	HORIZONTAL_BAR	= ChatColor.DARK_GRAY + "" + ChatColor.STRIKETHROUGH + Strings.repeat("-", 50);
+
+	private boolean				ENABLED			= true;
+	private FileConfiguration	config;
 
 	public void onEnable() {
 
 		getServer().getPluginManager().registerEvents(this, this);
 
+		loadConfig();
+		
+		getCommand("SpamClicker").setExecutor(this);
+
 		getLogger().info("SpamClicker v" + getDescription().getVersion() + " loaded. Click click click click click click :)");
+
+	}
+
+	public void loadConfig() {
+
+		File configFile = getFile("config.yml");
+		if (!configFile.exists()) {
+			saveResource("config.yml", true);
+		}
+
+		reloadConfig();
+		config = getConfig();
+
+		ENABLED = config.getBoolean("enabled");
 
 	}
 
@@ -25,12 +59,84 @@ public class SpamClicker extends JavaPlugin implements Listener {
 
 	}
 
+	public File getFile(String path) {
+
+		return new File(getDataFolder() + File.separator + path);
+
+	}
+
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerLogIn(PlayerJoinEvent e) {
 
-		Player p = e.getPlayer();
-		p.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(1024);
+		apply(e.getPlayer());
+
+	}
+
+	public void apply(Player p) {
+
+		p.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(ENABLED ? 1024 : 4);
 		p.saveData();
+
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+		if (!command.getLabel().equals("spamclicker")) {
+
+		return true;
+
+		}
+
+		if (args.length < 1) {
+
+			showHelp(sender);
+
+		} else {
+
+			if (args[0].equalsIgnoreCase("reload")) {
+
+				boolean oldEnabled = ENABLED;
+
+				loadConfig();
+
+				if (oldEnabled != ENABLED) {
+
+					for (Player p : Bukkit.getServer().getOnlinePlayers()) {
+
+						apply(p);
+
+					}
+
+				}
+
+				sender.sendMessage(ChatColor.GREEN + "SpamClicker" + ChatColor.AQUA + " config reloaded");
+
+			} else if (args[0].equals("version")) {
+
+				sender.sendMessage(ChatColor.AQUA + "You're running " + ChatColor.GREEN + "v" + getDescription().getVersion() + ChatColor.AQUA + " of " + ChatColor.GREEN + "SpamClicker");
+
+			} else {
+
+				sender.sendMessage(ChatColor.RED + "Unknown command '" + args[0] + "'");
+				showHelp(sender);
+
+			}
+
+		}
+
+		return true;
+
+	}
+
+	public void showHelp(CommandSender sender) {
+
+		sender.sendMessage(HORIZONTAL_BAR);
+
+		sender.sendMessage(ChatColor.GREEN + "/SpamClicker reload: " + ChatColor.GRAY + "Reloads the config");
+		sender.sendMessage(ChatColor.GREEN + "/SpamClicker version: " + ChatColor.GRAY + "Shows the version of SpamClicker");
+
+		sender.sendMessage(HORIZONTAL_BAR);
 
 	}
 
